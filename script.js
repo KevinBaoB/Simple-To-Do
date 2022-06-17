05be997ba13234575080fd37af356d41b2967f9e
KevinBaoB <kevinbaobui@gmail.com>
2022-06-16 19:06:00 -0500

    Naps help

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-let list = false;
-
 const add_list_item = function () {
-  const input = " " + document.querySelector(".form-control").value;
+  const input = document.querySelector(".form-control");
   const listing = document.querySelector(".listing");
-  const make_li = document.createElement("li");
+  const make_li = document.createElement("p");
   let checkbox = document.createElement("input");
   checkbox.type = "checkbox";
 
-  make_li.appendChild(checkbox);
-
-  make_li.appendChild(document.createTextNode(input));
+  make_li.innerText = " " + input.value;
+  make_li.prepend(checkbox);
   make_li.classList.add("checker");
   listing.appendChild(make_li);
 
-  document.querySelector(".container").appendChild(make_li);
-  list = true;
-  console.log(list);
+  checkbox.addEventListener("click", function () {
+    make_li.style.textDecoration = "line-through";
+  });
+  make_li.addEventListener("dblclick", function () {
+    make_li.style.textDecoration = "line-through";
+    listing.removeChild(make_li);
+  });
 };
 
 const crossOut = function () {
-  const text = document.querySelector(".checker").textContent;
   //   text.classList.add("strike");
   console.log(text);
 };
 
 document.querySelector(".btn-primary").addEventListener("click", add_list_item);
-document.querySelector(".checker").addEventListener("click", crossOut);
